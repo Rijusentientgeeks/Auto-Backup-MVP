939e4075bd66f3ae3b2bc692897677f95c41a0cb
Sk Shaifat Murshed <shaifat.murshed@sentientgeeks.com>
2025-03-17 10:59:53 +0530

    Implemented Backup Storage Component

--- angular/src/app/Storage/manage-storage/manage-storage.component.ts
+++ angular/src/app/Storage/manage-storage/manage-storage.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+// import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-manage-storage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manage-storage.component.html',
   styleUrl: './manage-storage.component.css'
 })
 export class ManageStorageComponent {
-
+  storages = [
+    { name: 'File Backup', icon: 'pi pi-folder', color: '#FFC107', description: 'Backup and restore your files securely.' },
+    { name: 'Database Backup', icon: 'pi pi-database', color: '#4CAF50', description: 'Ensure your database is safely stored.' },
+    { name: 'Application Backup', icon: 'pi pi-cloud', color: '#03A9F4', description: 'Save and restore application states.' }
+  ];
 }
