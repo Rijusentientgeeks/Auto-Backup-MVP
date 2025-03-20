7edfebb8056a3934cbd34b4515fc5e98ccc34387
Prasenjit Das <prasenjit@sentientgeeks.com>
2025-03-20 10:58:07 +0530

    missing dto file added

---
+++ aspnet-core/src/GeekathonAutoSync.Application/BackUpSchedules/Dto/BackUpScheduleCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekathonAutoSync.BackUpSchedules.Dto
+{
+    public class BackUpScheduleCreateDto
+    {
+        public Guid? SourceConfiguationId { get; set; }
+        public DateTime? BackupDate { get; set; }
+        public TimeSpan? BackupTime { get; set; }
+        public Guid? BackUpFrequencyId { get; set; }
+    }
+}
---
+++ aspnet-core/src/GeekathonAutoSync.Application/BackUpSchedules/Dto/GetBackUpScheduleInput.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Runtime.Validation;
+using GeekathonAutoSync.Shared.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekathonAutoSync.BackUpSchedules.Dto
+{
+    public class GetBackUpScheduleInput : PagedAndSortedInputDto, IShouldNormalize, IGetBackUpScheduleInput
+    {
+        public string FilterText { get; set; }
+        public void Normalize()
+        {
+            if (string.IsNullOrEmpty(Sorting))
+            {
+                Sorting = "CreationTime DESC";
+            }
+
+            FilterText = FilterText?.Trim();
+        }
+    }
+}
