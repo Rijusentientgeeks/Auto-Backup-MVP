303427aea7495e207f251c45d378bbb7949e8e09
Sk Shaifat Murshed <shaifat.murshed@sentientgeeks.com>
2025-04-09 18:30:18 +0530

    Modified and fixed bugs of Schedule backup & Source configuration. Added Loader

--- angular/src/app/Storage/manage-storage/storage-detail.component.ts
+++ angular/src/app/Storage/manage-storage/storage-detail.component.ts
@@ -9,7 +9,12 @@ import {
 import { Router } from "@node_modules/@angular/router";
 import { LazyLoadEvent } from "@node_modules/primeng/api";
 import { BackupService } from "@shared/service-proxies/backup-download.service";
-import { AutoBackupServiceProxy, BackUpLogDto, BackUpStorageConfiguationServiceProxy, SourceConfiguationDto } from "@shared/service-proxies/service-proxies";
+import {
+  AutoBackupServiceProxy,
+  BackUpLogDto,
+  BackUpStorageConfiguationServiceProxy,
+  SourceConfiguationDto,
+} from "@shared/service-proxies/service-proxies";
 
 @Component({
   selector: "app-storage-detail",
@@ -28,20 +33,19 @@ export class StorageDetailComponent implements OnChanges, OnInit {
   showBackupLogDialog: boolean = false;
   selectedStorageConfigurationId: string;
 
-  constructor(private backUpStorageConfiguationService: BackUpStorageConfiguationServiceProxy,
+  constructor(
+    private backUpStorageConfiguationService: BackUpStorageConfiguationServiceProxy,
     private autoBackupService: AutoBackupServiceProxy,
     private backupDownloadService: BackupService,
     private router: Router
-  ) { }
+  ) {}
   ngOnInit(): void {
-    ;
     var res = this.entries;
   }
   ngOnChanges(): void {
     this.filterData();
   }
   filterData(): void {
-    ;
     this.filteredEntries = this.entries.filter(
       (entry) =>
         entry.storageMasterType?.name.toLowerCase() ===
@@ -72,67 +76,67 @@ export class StorageDetailComponent implements OnChanges, OnInit {
   }
   getLabel(key: string): string {
     const labelMap: { [key: string]: string } = {
-      awS_AccessKey: 'Access Key',
-      awS_SecretKey: 'Secret Key',
-      awS_BucketName: 'Bucket Name',
-      awS_Region: 'Region',
-      awS_backUpPath: 'Backup Path',
-      aZ_AccountName: 'Account Name',
-      aZ_AccountKey: 'Account key',
-      nfS_IP: 'IP Address',
-      nfS_AccessUserID: 'Access user ID',
-      nfS_Password: 'Password',
-      nfS_LocationPath: 'Location Path'
+      awS_AccessKey: "Access Key",
+      awS_SecretKey: "Secret Key",
+      awS_BucketName: "Bucket Name",
+      awS_Region: "Region",
+      awS_backUpPath: "Backup Path",
+      aZ_AccountName: "Account Name",
+      aZ_AccountKey: "Account key",
+      nfS_IP: "IP Address",
+      nfS_AccessUserID: "Access user ID",
+      nfS_Password: "Password",
+      nfS_LocationPath: "Location Path",
     };
 
-    return labelMap[key] || key
-      .replace(/([A-Z])/g, ' $1')
-      .replace(/^./, (str) => str.toUpperCase())
-      .replace('Nf S', 'Network File System')
-      .replace('Aw S', 'AWS')
-      .replace('Az', 'Azure');
+    return (
+      labelMap[key] ||
+      key
+        .replace(/([A-Z])/g, " $1")
+        .replace(/^./, (str) => str.toUpperCase())
+        .replace("Nf S", "Network File System")
+        .replace("Aw S", "AWS")
+        .replace("Az", "Azure")
+    );
   }
   goBack() {
     this.back.emit();
   }
 
   GetDetailsbyID(entry: string) {
-    debugger
     this.backUpStorageConfiguationService.get(entry).subscribe({
       next: (result) => {
         this.editEntry.emit(result);
-      }
+      },
     });
   }
 
   deleteEntryDetails(id: string) {
     this.backUpStorageConfiguationService.delete(id).subscribe({
       next: () => {
-        debugger
         this.back.emit();
-      }
+      },
     });
   }
 
   openBackupLogDialog(id: string) {
     this.selectedStorageConfigurationId = id;
     this.showBackupLogDialog = true;
-  
+
     // Manually trigger initial data load
     const initialLazyLoadEvent: LazyLoadEvent = {
       first: 0,
-      rows: 10
+      rows: 10,
     };
     this.getSuccessullBackupLogLazy(initialLazyLoadEvent);
   }
 
-  
   getSuccessullBackupLogLazy(event: LazyLoadEvent) {
     if (!this.showBackupLogDialog) return;
 
     const skipCount = event.first ?? 0;
     const maxResultCount = event.rows ?? 10;
-  
+
     this.autoBackupService
       .getAllCompletedBackupLogByStorageConfigId(
         this.selectedStorageConfigurationId,
@@ -149,7 +153,7 @@ export class StorageDetailComponent implements OnChanges, OnInit {
         },
       });
   }
-  
+
   // getSuccessullBackupLogLazy(event: LazyLoadEvent) {
   //   this.autoBackupService.getAllCompletedBackupLogByStorageConfigId(this.selectedStorageConfigurationId, event.first, event.rows).subscribe({
   //     next: (result) => {
@@ -180,13 +184,10 @@ export class StorageDetailComponent implements OnChanges, OnInit {
   // }
 
   downloadBackup(backupLog: BackUpLogDto) {
-    debugger
     // this.backupDownloadService.downloadBackup(backupLog.sourceConfiguationId, backupLog.backUpFileName);
     this.backupDownloadService.downloadBackup({
       storageConfigurationId: backupLog.backUpStorageConfiguationId,
       backUpFileName: backupLog.backUpFileName,
     });
-    
   }
-  
 }
--- angular/src/app/schedule-backup/schedule-backup.module.ts
+++ angular/src/app/schedule-backup/schedule-backup.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ScheduleBackupRoutingModule } from './schedule-backup-routing.module';
-import { ScheduleBackupComponent } from './schedule-backup.component';
-import { DropdownModule } from 'primeng/dropdown';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
-import { DialogModule } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { SourceConfiguationServiceProxy } from '@shared/service-proxies/service-proxies';
-
+import { ScheduleBackupRoutingModule } from "./schedule-backup-routing.module";
+import { ScheduleBackupComponent } from "./schedule-backup.component";
+import { DropdownModule } from "primeng/dropdown";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
+import { InputTextModule } from "primeng/inputtext";
+import {
+  BackUpFrequencyServiceProxy,
+  BackUpScheduleServiceProxy,
+  SourceConfiguationServiceProxy,
+} from "@shared/service-proxies/service-proxies";
+import { MessageService } from "primeng/api";
+import { ToastModule } from "primeng/toast";
 
 @NgModule({
   declarations: [ScheduleBackupComponent],
@@ -17,12 +22,18 @@ import { SourceConfiguationServiceProxy } from '@shared/service-proxies/service-
     CommonModule,
     ScheduleBackupRoutingModule,
     DropdownModule,
-      DialogModule,
-        ReactiveFormsModule,
-           InputTextModule,
-            ButtonModule,
+    DialogModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
   exports: [ScheduleBackupComponent],
-  providers: [SourceConfiguationServiceProxy]
+  providers: [
+    SourceConfiguationServiceProxy,
+    BackUpFrequencyServiceProxy,
+    BackUpScheduleServiceProxy,
+    MessageService,
+  ],
 })
-export class ScheduleBackupModule { }
+export class ScheduleBackupModule {}
